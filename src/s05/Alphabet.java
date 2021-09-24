27097d1d11007cb1bd08e7cb3d43ba6c40a28e86
maltyncev <maltyncev@gmail.com>
2021-09-24 21:57:07 +0500

    29/09 22:00

--- src/s05/Alphabet.java
+++ src/s05/Alphabet.java
@@ -1,4 +1,19 @@
 package s05;
 
-public enum Alphabet {A, B, C, D
+public enum Alphabet {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+
+//    public String letter;
+//
+//    Alphabet(String letter) {
+//        this.letter = letter;
+//    }
+//
+//    public String getLetter() {
+//        return letter;
+//    }
+//
+//    public int index() {
+//        int index = Alphabet.valueOf(letter).ordinal() + 1;
+//        return index;
+//    }
 }
--- src/s05/T04.java
+++ src/s05/T04.java
@@ -1,8 +1,20 @@
 package s05;
 
+import java.util.Scanner;
+
 public class T04 {
 
     public static void main(String[] args) {
-	// write your code here
+        String name = requestString();
+        Alphabet letter = Alphabet.valueOf(name);
+        int index = letter.ordinal() + 1;
+        System.out.println(name + " - это " + index + "-я буква латинского алфавита");
+    }
+
+
+    static String requestString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите букву латинского алфавита: ");
+        return scanner.nextLine();
     }
 }
