118425b037e8292ca31c471581c7d31541211e0e
maltyncev <maltyncev@gmail.com>
2021-10-04 22:27:06 +0500

    04/10 22:25

--- src/s05/t02/Multiplication.java
+++ src/s05/t02/Multiplication.java
@@ -1,9 +1,6 @@
 package s05.t02;
 
 public class Multiplication extends Operation {
-//    Multiplication (int leftOperand, int rightOperand) {
-//        super();
-//    }
 
     @Override
     int doOperation(int leftOperand, int rightOperand) {
--- src/s05/t02/T02.java
+++ src/s05/t02/T02.java
@@ -1,7 +1,5 @@
 package s05.t02;
 
-import java.util.Scanner;
-
 public class T02 {
     public static void main(String[] args) {
 
