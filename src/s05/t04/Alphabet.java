992d815b601507e1bc296fe996bcb983754c9957
Mikhail Altyntsev <maltyncev@gmail.com>
2021-10-05 12:46:58 +0500

    S05-T04

--- src/s05/t04/Alphabet.java
+++ src/s05/t04/Alphabet.java
@@ -1,18 +1,8 @@
 package s05.t04;
 
-import java.util.Arrays;
-
 public enum Alphabet {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
 
-    public static int getPosition (String str) {
-        Alphabet letter = Alphabet.valueOf(str);
-        int position = letter.ordinal() + 1;
-        return position;
+     int getPosition () {
+        return ordinal() + 1;
     }
-
-    public static boolean strCheck (String str) {
-        String list = Arrays.toString(Alphabet.values());
-        return list.contains(str);
-    }
-
 }
--- src/s05/t04/T04.java
+++ src/s05/t04/T04.java
@@ -1,22 +1,8 @@
 package s05.t04;
 
-import java.util.Scanner;
-
 public class T04 {
 
     public static void main(String[] args) {
-        String str = requestString().toUpperCase();
-        if (Alphabet.strCheck(str)) {
-            int position = Alphabet.getPosition(str);
-            System.out.println(str + " - это " + position + "-я буква латинского алфавита");
-        } else {
-            System.out.println("Введенный символ не является буквой латинского алфавита");
-        }
-    }
-
-    static String requestString() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите букву латинского алфавита: ");
-        return scanner.nextLine();
+        Alphabet.F.getPosition();
     }
 }
